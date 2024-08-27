ce831ece2f6a483e5ab5d190af19f7136a5eb1e5
we1dy <ladyarboleda26@gmail.com>
2024-08-28 01:30:57 +0800

    Add project files.

---
+++ namespace/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace check1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.Write("Enter a Number: ");
+			int i = int.Parse(Console.ReadLine());
+
+			if (i % 2 == 0)
+			{
+				Console.Write("Entered Number is an Even Number");
+			}
+			else
+			{
+				Console.Write("Entered Number is an Odd Number");
+			}
+
+			Console.ReadKey();  // Keeps the console open until a key is pressed
+		}
+	}
+}
